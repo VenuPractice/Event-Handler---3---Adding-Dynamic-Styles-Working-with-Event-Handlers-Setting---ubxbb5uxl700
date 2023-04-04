2afc51548c40927a90219113cd64d1e38e2efc6c
Venu <116992247+VenuPractice@users.noreply.github.com>
2023-04-04 12:53:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ const App = () => {
 
   const handleInput = (event) =>{
    // use console.log
+    console.log('Input in #'+ event.target.id+ ' is ' +event.target.value)
   }
 
   // do not change id of input elements
